dedce9ea044fbe756a0accc70d721f4836ad70a4
Vyacheslav <vyacheslav@MBP-Vyacheslav.local>
2022-05-16 16:40:51 +0400

    fix: fixed toLocaleString

--- src/utils/toLocaleString.ts
+++ src/utils/toLocaleString.ts
@@ -1,7 +1,17 @@
 import BigNumber from 'bignumber.js'
 
 export const toLocaleString = (bn:BigNumber, b=false) => {
-  const str = Number(bn.toFixed(2)).toLocaleString('ru-Ru')
+  let dec = 2
+
+  if (bn.lte(0.001)) {
+    dec = 8
+  } else if (bn.lte(0.01)) {
+    dec = 6
+  } else if (bn.lte(1)) {
+    dec = 4
+  }
+
+  const str = Number(bn.toFixed(dec)).toLocaleString('ru-Ru')
 
   if (b) {
     return `***${str}***`
