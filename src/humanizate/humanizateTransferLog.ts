372d20d497ead8c86857c75120b8aec08a0512f9
Vyacheslav <vyacheslav@MBP-Vyacheslav.local>
2022-05-16 15:32:37 +0400

    feat: added NOMINEX_MONEY_BOX_WALLET

--- src/humanizate/humanizateTransferLog.ts
+++ src/humanizate/humanizateTransferLog.ts
@@ -6,6 +6,7 @@ import { whalesChatId } from '../config/constants/telegram'
 import { getButtons } from './getButtons'
 
 const NOMINEX_HOT_WALLET = '0x90aFDA51611523bE69abD37e26C2bf9162ACb797'
+const NOMINEX_MONEY_BOX_WALLET = '0xcD58a3928a162C6ec8A7Fe4c9537a322c023af6E'
 
 export const humanizateTransferLog = (log:TransferEvent):MessagePayloadType => {
   const price = targetPriceFetcher.getPrice()
@@ -14,7 +15,9 @@ export const humanizateTransferLog = (log:TransferEvent):MessagePayloadType => {
   let text:string
 
   if (log.from === NOMINEX_HOT_WALLET) {
-    text = `📩 #Отправлено ${toLocaleString(log.amount)} ${symbol} (~ $${toLocaleString(amountCost, true)}) из NOMINEX\_HOT\_WALLET в \`\`\`${log.to}\`\`\``
+    text = `📩 #Отправлено ${toLocaleString(log.amount)} ${symbol} (~ $${toLocaleString(amountCost, true)}) из NOMINEX HOT WALLET в \`\`\`${log.to}\`\`\``
+  } else if (log.from === NOMINEX_MONEY_BOX_WALLET) {
+    text = `📩 #Отправлено ${toLocaleString(log.amount)} ${symbol} (~ $${toLocaleString(amountCost, true)}) из NOMINEX MONEY BOX WALLET в \`\`\`${log.to}\`\`\``
   } else if (log.from === '0x0000000000000000000000000000000000000000') {
     text = `🪄 #Отчеканено ${toLocaleString(log.amount)} ${symbol} (~ $${toLocaleString(amountCost, true)})`
   } else {
