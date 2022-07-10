20461efed30d4c8d4214fc440dd2d21f8937cc26
Vyacheslav <vyacheslav@MacBook-Pro-Vyacheslav.local>
2022-07-10 23:22:14 +0400

    feat: update tg ids

--- src/projects/projects/nmx/telegram.ts
+++ src/projects/projects/nmx/telegram.ts
@@ -2,7 +2,7 @@ import type { TelegramConfigType } from '@/entries'
 import env from 'env-var'
 
 export const telegram:TelegramConfigType = {
-  whalesChatId: '1981691657', // '-1001520809718',
-  publicWhalesChatId: '1981691657',
+  whalesChatId: '-1001520809718',
+  publicWhalesChatId: '-1001765444286',
   botToken: env.get('NMX_TELEGRAM_BOT_TOKEN').required().asString(),
 }
