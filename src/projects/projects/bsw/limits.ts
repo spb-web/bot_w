2fd07ae1a5a0ec9686e3a77f2cc444d25e2e9716
Vyacheslav <vyacheslav@MacBook-Pro-Vyacheslav.local>
2022-07-11 02:03:47 +0400

    feat: update bsw limits

--- src/projects/projects/bsw/limits.ts
+++ src/projects/projects/bsw/limits.ts
@@ -1,22 +1,22 @@
 import type { ProjectLimitsType, ProjectMinLimitsType } from "@/entries"
 
 export const limits:ProjectLimitsType = {
+  minTransferAmountPrice: 5000,
+  minSwapAmountPrice: 5000,
+  minSwapAmountPriceWithLargeBalance: 5000,
+  minLpAmountPrice: 5000,
+  rewardAmountPrice: 5000,
+  stakeLpAmountPrice: 5000,
+  balanceAlertAmount: 5000,
+}
+
+export const publicLimits:ProjectLimitsType = {
   minTransferAmountPrice: 10000,
   minSwapAmountPrice: 10000,
   minSwapAmountPriceWithLargeBalance: 10000,
   minLpAmountPrice: 10000,
   rewardAmountPrice: 10000,
   stakeLpAmountPrice: 10000,
-  balanceAlertAmount: 10000,
-}
-
-export const publicLimits:ProjectLimitsType = {
-  minTransferAmountPrice: 100000,
-  minSwapAmountPrice: 100000,
-  minSwapAmountPriceWithLargeBalance: 100000,
-  minLpAmountPrice: 100000,
-  rewardAmountPrice: 100000,
-  stakeLpAmountPrice: 100000,
   balanceAlertAmount: 10000,
 }
 
