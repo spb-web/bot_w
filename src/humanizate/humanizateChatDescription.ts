e9632008041b84b22928950377aa215d52a3afc9
Vyacheslav <vyacheslav@MacBook-Pro-Vyacheslav.local>
2022-07-11 14:46:33 +0400

    feat: update ChatDescription

--- src/humanizate/humanizateChatDescription.ts
+++ src/humanizate/humanizateChatDescription.ts
@@ -3,10 +3,10 @@ import { toLocaleString } from "@/utils/toLocaleString"
 
 export const humanizateChatDescription = (project: ProjectType) => {
   return `
-- Transfers: $${toLocaleString(project.limits.minTransferAmountPrice)}
-- Swaps: $${toLocaleString(project.limits.minSwapAmountPrice)}
-- Mint/Burn liquidity: $${toLocaleString(project.limits.minLpAmountPrice)}
-- Deposit/Withdrawal: $${toLocaleString(project.limits.stakeLpAmountPrice)}
-- Farming rewards: $${toLocaleString(project.limits.rewardAmountPrice)}
+- 📩 Transfers: $${toLocaleString(project.limits.minTransferAmountPrice)} \n
+- 🐮🐻 Swaps: $${toLocaleString(project.limits.minSwapAmountPrice)} \n
+- 🔴🟢 Mint/Burn liquidity: $${toLocaleString(project.limits.minLpAmountPrice)}  \n
+- 👍👎 Deposit/Withdrawal: $${toLocaleString(project.limits.stakeLpAmountPrice)}  \n
+- 🤝 Farming rewards: $${toLocaleString(project.limits.rewardAmountPrice)} \n
   `.trim()
 }
